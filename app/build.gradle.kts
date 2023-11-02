6d4fe25832d349430e2b7a59cd28ab1622faca17
korbandras <korbandras@gmail.com>
2023-11-02 19:07:12 +0100

    as D

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
     implementation("androidx.recyclerview:recyclerview:1.3.0")
     implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.firebase:firebase-crashlytics-buildtools:2.9.9")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/example/myapplication/ToDo_Adaptor.kt
+++ app/src/main/java/com/example/myapplication/ToDo_Adaptor.kt
@@ -1,5 +1,7 @@
 package com.example.myapplication
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
 import android.view.LayoutInflater
 import android.view.View
@@ -8,10 +10,9 @@ import android.widget.CheckBox
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-
-
 class ToDo_Adaptor(
-    private val todos: MutableList<ToDo>
+    private var todos: MutableList<ToDo>,
+    private val context: Context
 ) : RecyclerView.Adapter<ToDo_Adaptor.ToDoViewHolder>() {
 
     class ToDoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -24,16 +25,18 @@ class ToDo_Adaptor(
             LayoutInflater.from(parent.context).inflate(R.layout.item_todo, parent, false)
         )
     }
-    fun addToDo(todo: ToDo){
+    fun addToDo(todo: ToDo) {
         todos.add(todo)
         notifyItemInserted(todos.size - 1)
+        saveToDoList(todos)
     }
 
-    fun deleteDone(){
+    fun deleteDone() {
         todos.removeAll { todo ->
             todo.isChecked
         }
         notifyDataSetChanged()
+        saveToDoList(todos)
     }
 
     private fun toggleStrikeThrough(tvToDoTitle: TextView, isChecked: Boolean){
@@ -60,5 +63,32 @@ class ToDo_Adaptor(
 
     override fun getItemCount(): Int {
         return todos.size
+    }
+
+    private val sharedPreferences: SharedPreferences = context.getSharedPreferences("todo_prefs", Context.MODE_PRIVATE)
+    private val todoListKey = "todo_list"
+
+    private fun loadToDoList(): MutableList<ToDo> {
+        val jsonList = sharedPreferences.getString(todoListKey, null)
+        return if (jsonList != null) {
+            val todoList = mutableListOf<ToDo>()
+            val todoStrings = jsonList.split(",")
+            for (todoString in todoStrings) {
+                val todo = ToDo(todoString)
+                todoList.add(todo)
+            }
+            todoList
+        } else {
+            mutableListOf()
+        }
+    }
+
+    private fun saveToDoList(todoList: MutableList<ToDo>) {
+        val jsonList = todoList.joinToString(",")
+        sharedPreferences.edit().putString(todoListKey, jsonList).apply()
+    }
+
+    init {
+        todos = loadToDoList()
     }
 }
